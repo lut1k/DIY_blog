ea91aea7093e26f82c10cb6afa9be749d3da6209
lut1k <lut1k@tut.by>
2019-11-18 17:55:15 +0300

    Home commit

--- mini_blog/blog/urls.py
+++ mini_blog/blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.home),
     re_path(r'^blogs/$', views.BlogListView.as_view(), name='blogs'),
     re_path(r'^bloggers/$', views.bloggers, name='bloggers'),
-    # re_path(r'^<int:pk>/$', views.BlogDetailView.as_view(), name="blog-detail")
+    re_path(r'^<int:pk>/$', views.BlogDetailView.as_view(), name="blog-detail")
 ]
--- mini_blog/blog/views.py
+++ mini_blog/blog/views.py
@@ -19,34 +19,16 @@ class BlogListView(generic.ListView):
     paginate_by = 5
     template_name = 'blog_list.html'
 
-    def dispatch(self, request, *args, **kwargs):
-        self.form = forms.BlogListForm(request.GET)
-        self.form.is_valid()
-        return super(BlogListView, self).dispatch(request, *args, **kwargs)
-        
-    def get_queryset(self):
-        queryset = models.Blog.objects.all()
-        if self.form.cleaned_data.get('search'):
-            queryset = queryset.filter(title__icontains=self.form.cleaned_data['search'])
-        if self.form.cleaned_data.get('sort_field'):
-            queryset = queryset.order_by(self.form.cleaned_data['sort_field'])
-        return queryset
-    
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(BlogListView, self).get_context_data(**kwargs)
-        context['form'] = self.form
-        return context
-
 
 def bloggers(request):
     return HttpResponse("Func bloggers")
 
 
-# class BlogDetailView(generic.DetailView):
-#     """
-#     Generic class-based detail view for a blog.
-#     """
-#     model = models.Blog
+class BlogDetailView(generic.DetailView):
+    """
+    Generic class-based detail view for a blog.
+    """
+    model = models.Blog
 
 # class AllBloggsView(generic.ListView):
 #     model = models.Blog
